7c4232d8daaed8cc8eaeed9c7a4d4ec9bc9bf9b6
guorong777 <guor783@gmail.com>
2026-08-14 10:19:23 +0800

    在GitHub修改HelloGit

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Git 第二个版本");
+        System.out.println("这是在GitHub远程仓库修改的");
     }
 }
